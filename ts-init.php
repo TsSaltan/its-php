c51a43ec09906dbae8e2c98837a3adac0b525d51
Alexey <tssaltan@gmail.com>
2018-08-17 15:38:02 +0300

    New constants

--- ts-init.php
+++ ts-init.php
@@ -2,6 +2,9 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('CD', __DIR__ . DS);
 
+if(!defined('STORAGE')) define('STORAGE', CD . 'storage' . DS);
+if(!defined('TEMP')) define('TEMP', STORAGE . 'temp' . DS);
+
 require 'ts-framework/Autoload.php';
 require 'vendor/autoload.php';
 
