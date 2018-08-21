15e2c42b52e36973247add737d524f5f0fdd9263
Alexey <tssaltan@gmail.com>
2018-08-21 13:28:35 +0300

    todo list

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ tsframe\App::start();
 
 /**
  * @@@ framework @@@
- * @todo изменение конфигов из админки
+ * @todo изменение конфигов из админки +
  * @todo utils to modules
  * @todo upload handler
  * @todo проверка домена при redirect
- * @todo user::get - мб переделать логику?
+ * @todo user::get - мб переделать логику? т.к. при проверке пароля обязательно передавать id
  * 
  */
