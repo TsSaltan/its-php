c451e5ba2c8dde926e2549aa7b5de879f57b9c1c
Alexey <tssaltan@gmail.com>
2018-08-26 16:26:23 +0300

    add user phone form to configs

--- ts-plugins/smsc/index.php
+++ ts-plugins/smsc/index.php
@@ -10,7 +10,9 @@ use tsframe\Hook;
 use tsframe\Plugins;
 use tsframe\module\menu\MenuItem;
 use tsframe\module\user\UserAccess;
+use tsframe\module\user\User;
 use tsframe\view\TemplateRoot;
+use tsframe\view\Template;
 
 Hook::register('app.install', function(){
 	if(is_null(Config::get('smsc'))){
@@ -22,10 +24,30 @@ Hook::register('app.install', function(){
 });
 
 Hook::registerOnce('plugin.load', function(){
-	Plugins::required('dashboard');
+	Plugins::required('dashboard', 'user', 'database', 'logger');
 	TemplateRoot::add('dashboard', __DIR__ . DS . 'template' . DS . 'dashboard');
 });
 
-Hook::register('menu.render.dashboard-sidebar', function(MenuItem $menu){
+/*Hook::register('menu.render.dashboard-sidebar', function(MenuItem $menu){
 	$menu->add(new MenuItem('SMS-сообщения', ['url' => Http::makeURI('/dashboard/sms'), 'fa' => 'envelope-o', 'access' => UserAccess::getAccess('smsc.log')]));
+});*/
+
+Hook::register('template.dashboard.user.edit', function(Template $tpl, array &$configTabs, int &$activeTab){
+	if(is_null($tpl->selectUser)) return;
+	$selectUser = $tpl->selectUser;
+
+	if($tpl->self || UserAccess::checkCurrentUser('user.edit')){
+		if(isset($_GET['phone'])){
+			$activeTab = sizeof($configTabs);
+		}
+
+		$configTabs['Телефон'] = function() use ($tpl, $selectUser){
+			$tpl->inc('user_phone');
+		};
+
+	}
+});
+
+Hook::register('template.render', function(Template $tpl){
+	$tpl->var('userPhone', User::current()->getMeta()->get('phone'));
 });
