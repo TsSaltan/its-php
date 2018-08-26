91c625c8da6c741cdad10f11f22234b8ba79ee40
Alexey <tssaltan@gmail.com>
2018-08-26 21:44:51 +0300

    Changed config template in dashboard

--- ts-plugins/dashboard/template/dashboard/config.tpl.php
+++ ts-plugins/dashboard/template/dashboard/config.tpl.php
@@ -7,22 +7,22 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header"><?=$this->title?></h1>
+                    <?showAlerts()?>
                 </div>
             </div>
             <?$this->hook('config')?>
             <div class="row">
                     <div class="col-lg-12">
-                    	<?uiAlert('Будьте осторожны при редактировании системного файла настроек!', 'warning')?>
-                    	<?showAlerts()?>
                         <div class="panel tabbed-panel panel-default">
                             <div class="panel-heading clearfix">
                                 <div class="panel-title pull-left">
-                                	Системный файл настроек
+                                    Системный файл настроек
                                 </div>
                             </div>
-                           	<form role="form" method="POST">
-	                            <div class="panel-body">
-	                            	<?uiJsonEditor($systemConfigs, 'config')?>
+                            <form role="form" method="POST">
+                                <div class="panel-body">
+                                    <?uiAlert('Будьте осторожны при редактировании системного файла настроек!', 'warning')?>
+	                                <?uiJsonEditor($systemConfigs, 'config')?>
 	                            </div>
 	                            <div class="panel-footer">
 	                           		<button class='btn btn-success'>Сохранить</button>
