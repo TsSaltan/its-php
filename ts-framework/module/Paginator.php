53d6bfa58ef56b18c8e4bf758809a8e3b063dcb0
Alexey <tssaltan@gmail.com>
2018-09-13 22:12:40 +0300

    Added Paginator::setDataCallback method and other

--- ts-framework/module/Paginator.php
+++ ts-framework/module/Paginator.php
@@ -34,6 +34,12 @@ class Paginator{
 	 */
 	protected $offset;
 
+	/**
+	 * Если нужно получить данные перед отображением, нужно установить данный коллбэк
+	 * @var boolean
+	 */
+	protected $getDataCallback = false;
+
 	public function __construct(array $data, int $num = 20){
 		$this->last = ceil(sizeof($data) / $num);
 		$this->page = min(max($_GET['page'] ?? 1, 1), $this->last);
@@ -48,11 +54,38 @@ class Paginator{
 	 * @return bool
 	 */
 	public function isData(): bool {
-		return sizeof($this->getData()) > 0;
+		return sizeof($this->getCurrentSlice()) > 0;
 	}
 
-	public function getData(): array {
+	/**
+	 * Установить callback функцию для получения данных
+	 * @param callable $callback function($currentValue, $currentKey)
+	 */
+	public function setDataCallback(callable $callback){
+		$this->getDataCallback = $callback;
+	}
+
+	/**
+	 * Получить "срез" массива - данные которые будут отображены на текущей странице
+	 * @return array
+	 */
+	protected function getCurrentSlice(): array {
 		return array_slice($this->data, $this->offset, $this->num);
+	}
+
+	/**
+	 * Получить массив данных для отображения
+	 * @return array
+	 */
+	public function getData(): array {
+		$data = $this->getCurrentSlice();
+		if(is_callable($this->getDataCallback)){
+			foreach ($data as $key => $value) {
+				$data[$key] = call_user_func($this->getDataCallback, $value, $key);
+			}
+		}
+
+		return $data;
 	}
 
 	public function getPages(int $pagesNum = 5, bool $helpers = true): array {
