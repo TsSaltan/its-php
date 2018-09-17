7ff19201d0901e996560a0d6d0c116c165a8f36d
Alexey <tssaltan@gmail.com>
2018-09-17 23:02:30 +0300

    Fixes in Meta class

--- ts-plugins/database/module/Meta.php
+++ ts-plugins/database/module/Meta.php
@@ -10,6 +10,27 @@ class Meta{
 
 	public function __construct(){
 		$this->parent = implode('_', func_get_args());
+	}	
+
+	/**
+	 * Чтоб сэкономить запросы, можно указать уже загруженные данные
+	 */
+	public function setLoadedData(array $data){
+		$this->data = $data;
+		$this->isLoaded = true;
+	}
+
+	/**
+	 * Флаг - были ли загружены данные ранее
+	 */
+	protected $isLoaded = false;
+
+	/**
+	 * Если данные не были загружены - грузим их из бд
+	 */
+	protected function loadData(){
+		if($this->isLoaded) return;
+
 		$data = Database::prepare('SELECT * FROM `meta` WHERE `parent` = :parent_id')
 				->bind('parent_id', $this->parent)
 				->exec()
@@ -18,9 +39,13 @@ class Meta{
 		foreach ($data as $value) {
 			$this->data[$value['key']] = $value['value'];
 		}
-	}	
+
+		$this->isLoaded = true;
+	}
 
 	public function set(string $key, $value){
+		$this->getData();
+
 		if(is_null($value) || strlen($value) == 0){
 			$query = Database::prepare('DELETE FROM `meta` WHERE (`parent` = :parent AND `key` = :key) OR `value` = null');
 		} else if(isset($this->data[$key])){
@@ -42,15 +67,16 @@ class Meta{
 	}
 
 	public function get(string $key){
-		return $this->data[$key] ?? null;
+		return $this->getData()[$key] ?? null;
 	}
 
 	public function getData(): array {
+		$this->loadData();
 		return $this->data;
 	}
 
 	public function isExists(): bool {
-		return sizeof($this->data) > 0;
+		return sizeof($this->getData()) > 0;
 	}
 
 	public function delete(): bool {
@@ -60,6 +86,24 @@ class Meta{
 						->affectedRows() > 0;
 	}
 	
+	public static function findByParent(string $parentMask): array {
+		$metas = Database::prepare('SELECT * FROM `meta` WHERE `parent` LIKE :parent')
+						->bind('parent', $parentMask)
+						->exec()
+						->fetch();
+		$found = [];
+		foreach ($metas as $meta) {
+			$found[$meta['parent']][$meta['key']] = $meta['value'];
+		}
+
+		foreach ($found as $parent => $data) {
+			$found[$parent] = new self($parent);
+			$found[$parent]->setLoadedData($data);
+		}
+
+		return $found;
+	}
+
 	public static function find(string $key = '*', string $value = '*'): array {
 		$return = [];
 		
