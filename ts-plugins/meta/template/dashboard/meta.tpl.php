ece170b7c8fa67a88b6182e8d017be4beef993b5
Alexey <tssaltan@gmail.com>
2018-09-29 19:58:24 +0300

    Meta: add form to editor

--- ts-plugins/meta/template/dashboard/meta.tpl.php
+++ ts-plugins/meta/template/dashboard/meta.tpl.php
@@ -22,46 +22,75 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading clearfix">
-                            <div class="panel-title pull-left" style="padding-top:6px">Редактор</div>
+                            <div class="panel-title pull-left">
+                                <ul class="nav nav-pills">
+                                    <li class="active"><a href="#meta-editor" data-toggle="tab">Редактор данных</a></li>
+                                    <li><a href="#meta-add" data-toggle="tab">Добавить запись</a></li>
+                                </ul>
+                            </div>
                             <div class="panel-title pull-right"><form action=""><input class="form-control" name='filter' placeholder="Фильтр" value="<?=$filter?>"></form></div>
                         </div>
-                    <?if($metaData->isData()):?>
                         <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-hover meta-editor">
-                                   <?foreach($metaData->getData() as $m):?>
-                                   <tr class='item-header'>     
-                                        <th colspan="2">
-                                            <code>
-                                            <?$parts = explode('_',  $m['parent'])?>
-                                            <?for($i = 0; $i < sizeof($parts); ++$i):?>
-                                                <?$part = implode('_', array_slice($parts, 0, $i+1))?>
-                                                <a class="" href="?filter=<?=$part?>"><?=$parts[$i]?></a>
-                                                <?if(isset($parts[$i+1])):?> &gt; <?endif?>
-                                            <?endfor?>
-                                            </code>
-                                        </th>
-                                    </tr>
-                                        <?foreach ($m['data'] as $key => $value):?>
-                                        <tr>  
-                                            <th style="padding-left:15px" width="120px"><?=$key?></th>    
-                                            <td>
-                                                <input class="form-control meta-field" data-parent="<?=$m['parent']?>" data-key="<?=$key?>" value="<?=$value?>" placeholder="delete"/>
-                                            </td>    
-                                        </tr>    
-                                        <?endforeach?>
-                                   <?endforeach?>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                        </div>
 
-                        <div class="panel-footer">
-                            <?foreach($metaData->getPages() as $page):?>
-                            <a class="btn btn-primary <?=($page['current'] ? "disabled" : "btn-outline")?>" href="<?=$page['url']?>"><?=$page['title']?></a>
-                            <?endforeach?>
+                            
+                            <div class="tab-content">
+                                <div class="tab-pane fade in active" id="meta-editor">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered table-hover meta-editor">
+                                           <?foreach($metaData->getData() as $m):?>
+                                           <tr class='item-header'>     
+                                                <th colspan="2">
+                                                    <code>
+                                                    <?$parts = explode('_',  $m['parent'])?>
+                                                    <?for($i = 0; $i < sizeof($parts); ++$i):?>
+                                                        <?$part = implode('_', array_slice($parts, 0, $i+1))?>
+                                                        <a class="" href="?filter=<?=$part?>"><?=$parts[$i]?></a>
+                                                        <?if(isset($parts[$i+1])):?> &gt; <?endif?>
+                                                    <?endfor?>
+                                                    </code>
+                                                </th>
+                                            </tr>
+                                                <?foreach ($m['data'] as $key => $value):?>
+                                                <tr>  
+                                                    <th style="padding-left:15px" width="120px"><?=$key?></th>    
+                                                    <td>
+                                                        <input class="form-control meta-field" data-parent="<?=$m['parent']?>" data-key="<?=$key?>" value="<?=$value?>" placeholder="delete"/>
+                                                    </td>    
+                                                </tr>    
+                                                <?endforeach?>
+                                           <?endforeach?>
+                                        </table>
+                                    </div>
+                                    <!-- /.table-responsive -->
+
+                                    <?if($metaData->hasPages()):?>
+                                        <div class="well">
+                                            <?foreach($metaData->getPages() as $page):?>
+                                            <a class="btn btn-primary <?=($page['current'] ? "disabled" : "btn-outline")?>" href="<?=$page['url']?>"><?=$page['title']?></a>
+                                            <?endforeach?>
+                                        </div>
+                                    <?endif?>
+                                </div>
+
+                                <div class="tab-pane fade" id="meta-add">
+                                    <form role="form" onsubmit="return addMeta(this)">
+                                        <div class="form-group">
+                                            <label>Parent</label>
+                                            <input class="form-control" name='parent' value='<?=$filter?>' required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Key</label>
+                                            <input class="form-control" name='key' required/>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Value</label>
+                                            <input class="form-control" name='value' required/>
+                                        </div>
+                                        <button class="btn btn-primary">Добавить</button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
-                    <?endif?>
                     </div>
                     <!-- /.panel -->
                 </div>
@@ -69,10 +98,24 @@
         </div>
     </div>
 <script type="text/javascript">
-    function saveMeta(parent, key, value){
+    function addMeta(form){
+        let $form = $(form);
+        var parent = document.querySelector('input[name=parent]').value;
+        let key = document.querySelector('input[name=key]').value;
+        let value = document.querySelector('input[name=value]').value;
+
+        saveMeta(
+            parent, key, value, 
+            function(){
+                document.location.replace('?filter=' + parent);
+            }
+        );
+    }
+
+    function saveMeta(parent, key, value, callback){
         let data = {key: key, parent: parent, value: value};
         console.log(data);
-        tsFrame.query('POST', 'meta', data);
+        tsFrame.query('POST', 'meta', data, callback);
     }
     
     $('.meta-field').blur(function(e){ 
