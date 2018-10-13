50e335f466cbdae0de2e12a4bb8437009be5238c
Alexey <tssaltan@gmail.com>
2018-10-13 19:44:46 +0300

    Logger sql query fix

--- ts-plugins/logger/install.sql
+++ ts-plugins/logger/install.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `log` (
 	`id` varchar(36) NOT NULL, `type` varchar(250) NOT NULL,
   	`data` text NOT NULL COMMENT 'JSON',
-  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
