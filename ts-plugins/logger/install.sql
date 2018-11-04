4955d6d39ae6611ab7e37a76cc76ec0baa13072a
Alexey <tssaltan@gmail.com>
2018-11-04 21:25:23 +0300

    Logger: added mysql indexes

--- ts-plugins/logger/install.sql
+++ ts-plugins/logger/install.sql
@@ -4,3 +4,6 @@ CREATE TABLE IF NOT EXISTS `log` (
   	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP INDEX IF EXISTS `type` ON `log`; -- MariaDB only
+ALTER TABLE `log` ADD INDEX(`type`);
