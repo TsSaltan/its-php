d9f4a08da1d1da5d23bd6c865f504d0cd543837c
Alexey <tssaltan@gmail.com>
2018-11-12 19:52:33 +0300

    Change log mysql install query

--- ts-plugins/logger/install.sql
+++ ts-plugins/logger/install.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `log` (
 	`id` varchar(36) NOT NULL, `type` varchar(250) NOT NULL,
   	`data` text NOT NULL COMMENT 'JSON',
-  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP INDEX IF EXISTS `type` ON `log`; -- MariaDB only
+--DROP INDEX IF EXISTS `type` ON `log`; -- MariaDB only
 ALTER TABLE `log` ADD INDEX(`type`);
