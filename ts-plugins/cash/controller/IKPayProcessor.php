cd8ab85fcd58e67cee1f6d3e4884f7fc08dbbce4
Ts.Saltan <TsSaltan@gmail.com>
2018-11-25 18:13:42 +0300

    Cash controller bug fixes

--- ts-plugins/cash/controller/IKPayProcessor.php
+++ ts-plugins/cash/controller/IKPayProcessor.php
@@ -15,20 +15,22 @@ class IKPayProcessor extends AbstractController{
 
 	public function response(){
 		$this->responseType = 'text/plain';
-		$t = time();
-		if(isset($_REQUEST['ik_inv_st']) && $_REQUEST['ik_inv_st'] == 'success' && $check = API::checkPayment($_REQUEST)){
-			//file_put_contents(TEMP.'pay_success_' . $t . '.txt', 'check => ' . var_export($check, true ) . "\n\n" . 'REQUEST = ' . var_export($_REQUEST, true) . "\n\n" . 'SERVER = ' . var_export($_SERVER, true));
 
+		if(isset($_REQUEST['ik_inv_st']) && $_REQUEST['ik_inv_st'] == 'success' && $check = API::checkPayment($_REQUEST)){
 			$am = $_REQUEST['ik_am'];
 			$description = $_REQUEST['ik_desc'];
 			$payId = $_REQUEST['ik_pm_no'];
 			$userId = explode('_', $payId)[1];
 
-			Log::Cash('Успешный запрос от платёжного сервера', ['pay_id' => $payId, 'check' => $check, 'request' => $_REQUEST]);
+			Log::Cash('Успешный запрос от платёжного сервера', [
+				'pay_id' => $payId, 
+				'check' => $check, 
+				'request' => $_REQUEST
+			]);
 
 			$cash = Cash::ofUserId($userId);
 			
-			if(!$cash->isTransactionExists()){
+			if(!$cash->isTransactionExists($payId)){
 				$cash->add($am, $description . '; Transaction ID: ' . $payId);
 				Hook::call('cash.pay', [$userId, $am, $description, $payId]);
 			} else {
@@ -38,9 +40,10 @@ class IKPayProcessor extends AbstractController{
 			Http::send('OK', 200);
 
 			die;
+		} elseif(isset($_REQUEST['ik_inv_st'])){
+			Log::Cash('Запрос от платёжного сервера', ['check' => $check, 'request' => $_REQUEST]);
 		}
 
-		Log::Cash('Запрос от платёжного сервера', ['check' => $check, 'request' => $_REQUEST]);
 	
 		return Http::redirect(Http::makeURI('/dashboard/user/me/edit?balance=frompay'));
 	}
