b82fcc6727f7ea10d6410ba5c9763deb0e91eedf
Ts.Saltan <TsSaltan@gmail.com>
2019-02-10 13:44:25 +0300

    Cron controller

---
+++ ts-framework/controller/Cron.php
@@ -0,0 +1,21 @@
+<?php
+namespace tsframe\controller;
+
+use tsframe\Config;
+use tsframe\Http;
+use tsframe\Plugins;
+
+/**
+ * Контроллер для планировщика задач
+ * @route /cron/[:key]
+ */
+class Cron extends AbstractController{
+
+	public function __construct(){
+		
+	}
+
+	public function response(){
+
+	}
+}
