2f8177b15cd9817970925c07c014c839ea64e92d
Ts.Saltan <TsSaltan@gmail.com>
2019-02-10 13:40:36 +0300

    App::Version 2019.1

--- ts-framework/App.php
+++ ts-framework/App.php
@@ -1,13 +1,19 @@
 <?php
 namespace tsframe;
 
+use tsframe\controller\ErrorController;
+use tsframe\controller\InstallController;
 use tsframe\exception\BaseException;
 use tsframe\exception\PluginException;
 use tsframe\view\Template;
-use tsframe\controller\ErrorController;
-use tsframe\controller\InstallController;
 
 class App{
+
+	/**
+	 * Версия приложения
+	 * Год.major.minor
+	 */
+	const VERSION = "2019.1.0";
 
 	/**
 	 * Путь к директории на сервере, откуда будет брать начало роутер, ссылки и т.д.
@@ -36,6 +42,18 @@ class App{
 
 	public static function getBasePath(): string {
 		return self::$basePath;
+	}
+
+	/**
+	 * Проверяет, установлена ли необходимая версия фреймворка
+	 * @param  string $version Требуемая версия
+	 * @param  string $rule    Правило для сравнения, по умлочанию >=
+	 * @throws BaseException
+	 */
+	public static function requiredVersion(string $version, string $rule = ">="){
+		if(!version_compare(self::VERSION, $version, $rule)){
+			throw new BaseException("Required framework version " . $rule . " " . $version . ". Current version: " . self::VERSION);
+		}
 	}
 
 	/**
