0cfa319dc0e9c90914923ad1b9181f005ad8363a
Ts.Saltan <TsSaltan@gmail.com>
2019-02-20 20:19:19 +0300

    add logging to vk-api plugin

--- ts-plugins/vk-api/controller/VKCallbackController.php
+++ ts-plugins/vk-api/controller/VKCallbackController.php
@@ -4,21 +4,26 @@ namespace tsframe\controller;
 use tsframe\Config;
 use tsframe\Hook;
 use tsframe\Http;
+use tsframe\module\Log;
 use tsframe\module\vk\CallbackAPI;
 
 /**
  * @route GET|POST /vk-callback/
  */ 
 class VKCallbackController extends AbstractController {
+	public static $log = true;
+
 	public function response(){
 		$this->responseType = Http::TYPE_PLAIN;
 
 		$confirmToken = Config::get('vk.confirmToken');
 		$gAccessToken = Config::get('vk.groupAccessToken');
 		$cbApi = new CallbackAPI($gAccessToken, $confirmToken);
-		
+
 		if($cbApi->hasData()){
 			$data = $cbApi->getInputData();
+			if($log) Log::VKCallback('Incoming query', $data);
+
 			switch($data['type']){
 				case 'confirmation':
 					return $cbApi->getConfirmToken();
@@ -30,6 +35,8 @@ class VKCallbackController extends AbstractController {
 					Hook::call('vk.' . $data['type'], [$cbApi->getApi(), $data]);
 			}
 			return "ok";
+		} else {
+			if($log) Log::VKCallback('Incoming null query', []);
 		}
 
 		return "null";
