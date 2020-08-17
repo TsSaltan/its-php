3eef8348da1ea8fd735198c89a2b4507f217a373
Alexey <tssaltan@gmail.com>
2020-08-17 18:08:24 +0300

    Logging cash exceptions

--- ts-plugins/cash/exception/CashException.php
+++ ts-plugins/cash/exception/CashException.php
@@ -2,7 +2,11 @@
 namespace tsframe\exception;
 
 use tsframe\exception\BaseException;
+use tsframe\module\Log;
 
 class CashException extends BaseException {
-    
+    public function __construct(string $message = null, int $code = 0, array $debugData = []){
+		Log::cash('[Error] ' . $message, ['type' => 'error', 'debug' => $debugData]);
+		return parent::__construct($message, $code, $debugData);
+	}
 }
