9811174c8a73e865d444d332ed5e1cb93e0f9c5f
Alexey <tssaltan@gmail.com>
2020-10-13 01:14:00 +0300

    Log mysql query fix

--- ts-plugins/logger/install.sql
+++ ts-plugins/logger/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `log` (
 	`id` varchar(36) NOT NULL, `type` varchar(250) NOT NULL,
   	`data` text NOT NULL COMMENT 'JSON',
-  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  	`date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
