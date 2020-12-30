1da774c4c365fb77caa398605d0e00f6dcbaa94b
Ts.Saltan <TsSaltan@gmail.com>
2020-12-30 17:36:51 +0300

    Remove deprecated functions

--- ts-plugins/dashboard-themes/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace tsframe;
-
-use tsframe\Plugins;
- // @deprecated
-Plugins::disable('dashboard-themes');
-throw new \Exception('Plugin "dashboard-themes" was deprecated! Reload this page ...');
