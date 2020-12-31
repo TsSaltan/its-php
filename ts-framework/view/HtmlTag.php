700c1a2d8569063fd8d683bc8b70b1277e35600b
Ts.Saltan <TsSaltan@gmail.com>
2020-12-31 19:10:32 +0300

    HtmlTag addElement override

--- ts-framework/view/HtmlTag.php
+++ ts-framework/view/HtmlTag.php
@@ -1,8 +1,26 @@
 <?php
 namespace tsframe\view;
 
-class HtmlTag extends \HtmlGenerator\HtmlTag {
+use HtmlGenerator\HtmlTag as HtmlGenTag;
+
+class HtmlTag extends HtmlGenTag {
 	public function getChild(): array {
 		return $this->content;
 	}
+
+	/**
+     * Добавляет элемент, но не клонирует его, как это указано в родительском классе
+     * @param Markup|string $tag
+     * @return static instance
+     * @override
+     */
+    public function addElement($tag = '')
+    {
+        $htmlTag = (is_object($tag) && ($tag instanceof self || $tag instanceof HtmlGenTag)) ? $tag : new static($tag);
+        $htmlTag->top = $this->getTop();
+        $htmlTag->parent = &$this;
+
+        $this->content[] = $htmlTag;
+        return $htmlTag;
+    }
 }
