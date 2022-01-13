2b1e08f2822490c78241bb4ff6b310ac7f015208
Alexey <tssaltan@gmail.com>
2022-01-13 14:14:10 +0300

    Dashboard config UI fix

--- its-plugins/dashboard/template/dashboard/config.tpl.php
+++ its-plugins/dashboard/template/dashboard/config.tpl.php
@@ -14,7 +14,7 @@
                 <form id="setmode_form" action="<?=$this->makeURI('/dashboard/config/setmode')?>" method="POST">  
                     <div class="col-lg-3">
                         <div class="form-group input-group">
-                            <span class="input-group-addon" style="width:150px">Dev mode</span>
+                            <span class="input-group-addon" style="width:50%">Dev mode</span>
                             <select class="form-control" name="dev_mode" onchange="$('#setmode_form').submit()">
                                 <option value="0">False</option>
                                 <option value="1" <?php if($dev_mode === true){ echo "selected"; }?>>True</option>
@@ -24,7 +24,7 @@
 
                     <div class="col-lg-3">
                         <div class="form-group input-group">
-                            <span class="input-group-addon" style="width:150px">Install mode</span>
+                            <span class="input-group-addon" style="width:50%">Install mode</span>
                             <select class="form-control" name="install_mode" onchange="$('#setmode_form').submit()">
                                 <option value="0">False</option>
                                 <option value="1"<?php if($install_mode === true){ echo "selected"; }?>>True</option>
