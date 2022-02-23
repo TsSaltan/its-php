709365f688227a20167d3fde5aff52ecb67f5b5c
Alexey <tssaltan@gmail.com>
2022-02-23 12:00:13 +0300

    Separate MetaDashboard functionality to dashboard-meta plugin

---
+++ its-plugins/dashboard-meta/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Dashboard страница для META редактора
+ */
+namespace tsframe;
+
+use tsframe\Config;
+use tsframe\Hook;
+use tsframe\Http;
+use tsframe\Plugins;
+use tsframe\module\menu\MenuItem;
+use tsframe\module\user\UserAccess;
+use tsframe\view\TemplateRoot;
+
+Hook::registerOnce('plugin.install', function(){
+	Plugins::required('dashboard', 'meta');
+	return [
+		PluginInstaller::withKey('access.meta')
+					->setType('select')
+					->setDescription("Права доступа: доступ к META-реестру")
+					->setDefaultValue(UserAccess::Admin)
+					->setValues(array_flip(UserAccess::getArray())),
+	];
+});
+
+Hook::registerOnce('app.init', function(){
+	TemplateRoot::add('dashboard', __DIR__ . DS . 'template' . DS . 'dashboard');
+});
+
+Hook::register('menu.render.dashboard-admin-sidebar', function(MenuItem $menu){
+	$menu->add(new MenuItem('Meta реестр данных', ['url' => Http::makeURI('/dashboard/meta'), 'fa' => 'table', 'access' => UserAccess::getAccess('meta')]), -2);
+});
--- its-plugins/meta/index.php
+++ its-plugins/meta/index.php
@@ -1,32 +1,12 @@
 <?php
 /**
- * META редактор
+ * META реестр в базе данных
  */
 namespace tsframe;
 
-use tsframe\Config;
 use tsframe\Hook;
-use tsframe\Http;
 use tsframe\Plugins;
-use tsframe\module\menu\MenuItem;
-use tsframe\module\user\UserAccess;
-use tsframe\view\TemplateRoot;
 
 Hook::registerOnce('plugin.install', function(){
-	Plugins::required('dashboard', 'database');
-	return [
-		PluginInstaller::withKey('access.meta')
-					->setType('select')
-					->setDescription("Права доступа: доступ к META-реестру")
-					->setDefaultValue(UserAccess::Admin)
-					->setValues(array_flip(UserAccess::getArray())),
-	];
-});
-
-Hook::registerOnce('app.init', function(){
-	TemplateRoot::add('dashboard', __DIR__ . DS . 'template' . DS . 'dashboard');
-});
-
-Hook::register('menu.render.dashboard-admin-sidebar', function(MenuItem $menu){
-	$menu->add(new MenuItem('Meta реестр данных', ['url' => Http::makeURI('/dashboard/meta'), 'fa' => 'table', 'access' => UserAccess::getAccess('meta')]), -2);
+	Plugins::required('database');
 });
