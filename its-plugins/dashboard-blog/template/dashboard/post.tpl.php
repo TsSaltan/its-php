f4f51afc2d9bd7d611a786f2847be98ed96b1da5
Alexey <tssaltan@gmail.com>
2022-03-19 16:57:29 +0300

    Tpl ui changes #10

--- its-plugins/dashboard-blog/template/dashboard/post.tpl.php
+++ its-plugins/dashboard-blog/template/dashboard/post.tpl.php
@@ -19,60 +19,49 @@
                     <form action="<?=$this->makeURI('/dashboard/blog/post/' . $post->getId() . '/save')?>" method="POST">                    
                         <div class="panel-body">
                             <div class="col-12 col-lg-8">  
-                                <div class="col-lg-12"> 
-                                    <div class="form-group">
-                                        <label><?=__('post-title')?></label>
-                                        <input class="form-control" name="title" type="text" value="<?=$post->getTitle()?>">
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-title')?></label>
+                                    <input class="form-control" name="title" type="text" value="<?=$post->getTitle()?>">
                                 </div>
           
-                                <div class="col-lg-12"> 
-                                    <div class="form-group">
-                                        <label><?=__('post-alias')?></label>
-                                        <input class="form-control" name="alias" type="text" value="<?=$post->getAlias()?>">
-                                        <p class="help-block"><?=__('post-alias-description')?></p>
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-alias')?></label>
+                                    <input class="form-control" name="alias" type="text" value="<?=$post->getAlias()?>">
+                                    <p class="help-block"><?=__('post-alias-description')?></p>
                                 </div>
 
-                                <div class="col-lg-12"> 
-                                    <div class="form-group">
-                                        <label><?=__('post-content')?></label>
-                                        <textarea class="form-control" name="content" rows="10" type="text"><?=$post->getContent(false)?></textarea>
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-content')?></label>
+                                    <textarea class="form-control" name="content" rows="10" type="text"><?=$post->getContent(false)?></textarea>
                                 </div>
                             </div>
 
                             <div class="col-12 col-lg-4">  
-                                <div class="col-12 col-lg-12">  
-                                    <div class="form-group">
-                                        <label><?=__('post-author')?></label>
-                                        <p>
-                                            <?php if($author->isAuthorized()): ?>
-                                            <a href="<?=$this->makeURI('/dashboard/user/' . $author->get('id'))?>" class="btn btn-default"><?=$author->get('login')?></a>
-                                            <?php else: ?>
-                                            <a href="#" class="btn btn-default btn-disabled" disabled><?=__('post-author-undefined')?></a>
-                                            <?php endif; ?>
-                                        </p>
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-author')?></label>
+                                    <p>
+                                        <?php if($author->isAuthorized()): ?>
+                                        <a href="<?=$this->makeURI('/dashboard/user/' . $author->get('id'))?>" class="btn btn-default"><?=$author->get('login')?></a>
+                                        <?php else: ?>
+                                        <a href="#" class="btn btn-default btn-disabled" disabled><?=__('post-author-undefined')?></a>
+                                        <?php endif; ?>
+                                    </p>
                                 </div>
                                 
-                                <div class="col-12 col-lg-12">  
-                                    <div class="form-group">
-                                        <label><?=__('post-timestamp')?></label>
-                                        <p class="help-block"><strong><?=__('post-create-time')?></strong>:<em><?=$post->getCreateTime()?></em></p>
-                                        <p class="help-block"><strong><?=__('post-update-time')?></strong>:<em><?=$post->getUpdateTime()?></em></p>
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-timestamp')?></label>
+                                    <p class="help-block"><strong><?=__('post-create-time')?></strong>:<em><?=$post->getCreateTime()?></em></p>
+                                    <p class="help-block"><strong><?=__('post-update-time')?></strong>:<em><?=$post->getUpdateTime()?></em></p>
                                 </div>
 
-                                <div class="col-12 col-lg-12">  
-                                    <div class="form-group">
-                                        <label><?=__('post-type')?></label>
-                                        <select class="form-control" name="type">
-                                            <option value="0" <?php if($post->isDraft()): ?>selected<?php endif; ?>><?=__('post-type-draft')?></option>
-                                            <option value="1" <?php if($post->isProduction()): ?>selected<?php endif; ?>><?=__('post-type-production')?></option>
-                                        </select>
-                                    </div>
+                                <div class="form-group">
+                                    <label><?=__('post-type')?></label>
+                                    <select class="form-control" name="type">
+                                        <option value="0" <?php if($post->isDraft()): ?>selected<?php endif; ?>><?=__('post-type-draft')?></option>
+                                        <option value="1" <?php if($post->isProduction()): ?>selected<?php endif; ?>><?=__('post-type-production')?></option>
+                                    </select>
                                 </div>
+
                             </div>
                         </div>
 
