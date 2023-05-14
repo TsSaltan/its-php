b191c1580133bc917962748f13dd7e8de218f456
Ts.Saltan <TsSaltan@gmail.com>
2023-05-14 17:15:57 +0300

    UI template fix

--- its-plugins/dashboard-blog/template/dashboard/category.tpl.php
+++ its-plugins/dashboard-blog/template/dashboard/category.tpl.php
@@ -11,35 +11,37 @@
             </div>
 
             <div class="row">
-                <div class="panel panel-default">
-                    <div class="panel-heading clearfix">
-                        <div class="panel-title pull-left">#<?=$category->getId();?>&nbsp;<strong><?=$category->getTitle()?></strong></div>
-                        <div class="panel-title pull-right">
-                            <a href="#" data-toggle="modal" data-target="#modal-delete" class="btn btn-danger btn-xs btn-outline"><i class="fa fa-trash"></i>&nbsp;<?=__('button/delete')?></a>
+                <div class="col-12 col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading clearfix">
+                            <div class="panel-title pull-left">#<?=$category->getId();?>&nbsp;<strong><?=$category->getTitle()?></strong></div>
+                            <div class="panel-title pull-right">
+                                <a href="#" data-toggle="modal" data-target="#modal-delete" class="btn btn-danger btn-xs btn-outline"><i class="fa fa-trash"></i>&nbsp;<?=__('button/delete')?></a>
+                            </div>
                         </div>
-                    </div>
 
-                    <form action="<?=$this->makeURI('/dashboard/blog/category/' . $category->getId())?>" method="POST">                    
-                        <div class="panel-body">
-                            <div class="col-12 col-lg-12">  
-                                <div class="form-group">
-                                    <label><?=__('category-title')?></label>
-                                    <input class="form-control" name="title" type="text" value="<?=$category->getTitle()?>">
+                        <form action="<?=$this->makeURI('/dashboard/blog/category/' . $category->getId())?>" method="POST">                    
+                            <div class="panel-body">
+                                <div class="col-12 col-lg-12">  
+                                    <div class="form-group">
+                                        <label><?=__('category-title')?></label>
+                                        <input class="form-control" name="title" type="text" value="<?=$category->getTitle()?>">
+                                    </div>
+              
+                                    <div class="form-group">
+                                        <label><?=__('category-alias')?></label>
+                                        <input class="form-control" name="alias" type="text" value="<?=$category->getAlias()?>">
+                                        <p class="help-block"><?=__('category-alias-description')?></p>
+                                    </div>
                                 </div>
-          
-                                <div class="form-group">
-                                    <label><?=__('category-alias')?></label>
-                                    <input class="form-control" name="alias" type="text" value="<?=$category->getAlias()?>">
-                                    <p class="help-block"><?=__('category-alias-description')?></p>
-                                </div>
+
                             </div>
 
-                        </div>
-
-                        <div class="panel-footer"><button type="submit" class="btn btn-primary"><?=__('button/save')?></button></div>
-                    </form>
+                            <div class="panel-footer"><button type="submit" class="btn btn-primary"><?=__('button/save')?></button></div>
+                        </form>
+                    </div>
+                    <!-- /.panel -->
                 </div>
-                <!-- /.panel -->
             </div>
 
             <?php $this->incCategories()?>
