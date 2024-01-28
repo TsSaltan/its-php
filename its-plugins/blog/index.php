c0a3bfdc281daf36a3760799b038d726f29a17ad
Ts.Saltan <TsSaltan@gmail.com>
2024-01-28 21:41:18 +0300

    Added "inc" hook for blog

--- its-plugins/blog/index.php
+++ its-plugins/blog/index.php
@@ -6,6 +6,23 @@
  */
 namespace tsframe;
 
+use tsframe\module\blog\Post;
+use tsframe\module\blog\Blog;
+
 Hook::registerOnce('plugin.install', function(){
 	Plugins::required('database');
+});
+
+Hook::registerOnce('blog.post.inc', function(Post $post, $params){
+	if(isset($params['id'])){
+		try {
+			$post = Blog::getPostById($params['id']);
+			echo $post->getContent(true);
+			return true;
+		} catch(\Exception $e){
+
+		}
+	}
+	echo null;
+	return false;
 });
